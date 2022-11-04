af56290c7f0bd7b4fc6e8becdc69d051df14afbb
Shrishaily007 <112272187+Shrishaily007@users.noreply.github.com>
2022-11-04 16:48:49 +0530

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -113,7 +113,7 @@ bot.callbackQuery("start", async (ctx) => {
       {
         reply_markup: new InlineKeyboard()
           .text(ctx.t("usage-help"), "helper").row()
-          .url(ctx.t("updates"), "https://t.me/BotzHub"),
+          .url(ctx.t("updates"), "https://t.me/Disney_villa"),
         disable_web_page_preview: true,
         parse_mode: "HTML",
       },
